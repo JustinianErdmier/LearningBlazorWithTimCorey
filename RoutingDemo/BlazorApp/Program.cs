6622d14ab36c1ccdeae10af8f373e0f0ef9db233
JustinErdmier <justin.erdmier@ltnow.com>
2022-06-30 12:29:27 -0500

    Update routing demo.

--- RoutingDemo/BlazorApp/Program.cs
+++ RoutingDemo/BlazorApp/Program.cs
@@ -12,7 +12,11 @@ builder.Services.AddSingleton<WeatherForecastService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
